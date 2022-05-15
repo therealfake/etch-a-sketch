bef09582d0e3cf4a2a7d3fa1e84479390450872a
Tianji Zhang <zhang.sh.tianji@gmail.com>
2022-05-15 17:10:38 -0400

    style grid

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const gridContainer = document.querySelector("#grid-container");
  * @param  {Number} cols number of columns
  */
 function makeGrid(rows, cols) {
+    gridContainer.style.setProperty('--grid-rows', rows);
+    gridContainer.style.setProperty('--grid-cols', cols);
     for (c = 0; c < (rows*cols); c++) {
         let cell = document.createElement("div");
         gridContainer.appendChild(cell).className = "grid-item";
